662482018851db42eab93b0e1486203bdf380c35
nathan ganesh <489290@student.saxion.nl>
2020-12-15 18:36:34 +0100

    first commit

---
+++ clinicalApi/src/test/java/com/ikiddoi/clinical/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikiddoi.clinical.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
